2f609c1e729a9d7100ca01b68d618c1fe8fe5229
Aditya2763 <singhaditya2763@gmail.com>
2024-10-29 14:49:22 +0530

    update

--- src/utils/redirectUrl.js
+++ src/utils/redirectUrl.js
@@ -1,5 +1,5 @@
-const redirectUrl = "https://bni-management-software-frontend-reactjs.vercel.app";
+// const redirectUrl = "https://bni-management-software-frontend-reactjs.vercel.app";
 
-// const redirectUrl = "http://localhost:5173";
+const redirectUrl = "http://103.169.86.3:5173";
 
 export default redirectUrl;
